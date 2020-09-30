4aa5ac18b7ef8908ffc5e0cad055b806f90a91ce
2bit-hack <sohamkar76@gmail.com>
2020-09-30 20:45:10 +0530

    Add initial validators

---
+++ validators/validators.js
@@ -0,0 +1,16 @@
+const validatePassword = (password) => {
+    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+
+    return passwordRegex.test(password);
+};
+
+const validateEmail = (email) => {
+    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    return emailRegex.test(email);
+};
+
+module.exports = {
+    validatePassword,
+    validateEmail,
+};
